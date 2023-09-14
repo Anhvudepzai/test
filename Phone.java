777d3a56489cbee9912b301bb8c80e9fd4e79252
AnhVu <anhvu019vta@gmail.com>
2023-09-14 20:47:36 +0700

    First commit

---
+++ Phone.java
@@ -0,0 +1,9 @@
+
+abstract public class Phone {
+	abstract void insertPhone(String name ,String phone);
+	abstract void removePhone(String name);
+	abstract void updatePhone(String name ,String newphone);
+	abstract void searchPhone(String name);
+	abstract void sort();
+	
+}
